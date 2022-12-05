39becf2b3f266b5eb47b855ea9c161c0d0f3dd78
GoodFaithParadigm8 <jason.a.sprouse@gmail.com>
2022-12-05 17:24:33 -0600

    cargo fmt

--- src/state.rs
+++ src/state.rs
@@ -3,22 +3,21 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Coin};
 use cw_storage_plus::{Item, Map};
 
-
 // The struct for a bid
 #[cw_serde]
 pub struct Bid {
-    pub amount: Coin
+    pub amount: Coin,
 }
-  
-  // The struct for a bidder
+
+// The struct for a bidder
 #[cw_serde]
 pub struct Bidder {
     pub sender: Addr,
     pub total_bid: Bid,
-    pub transfer_addr: Option<Addr>
+    pub transfer_addr: Option<Addr>,
 }
-  
-  // The state of the contract
+
+// The state of the contract
 pub const STATE: Item<State> = Item::new("state");
 pub const BIDDER: Map<&Addr, Bidder> = Map::new("bidders");
 
@@ -26,12 +25,10 @@ pub const BIDDER: Map<&Addr, Bidder> = Map::new("bidders");
 pub struct State {
     // The current highest bid
     pub highest_bid: Bidder,
-    
+
     // The address of the contract owner
     pub owner: Addr,
-    
+
     // closes the bidding
     pub closed_bidding: bool,
 }
-
-
