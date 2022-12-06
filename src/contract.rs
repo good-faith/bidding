21caabfa675faaf5b89808b91f3e37f3db3e4b58
GoodFaithParadigm8 <jason.a.sprouse@gmail.com>
2022-12-05 21:17:46 -0600

    ~ inistiatiate fn, ~ msgs

--- src/contract.rs
+++ src/contract.rs
@@ -1,12 +1,12 @@
 #[cfg(not(feature = "library"))]
 use crate::msg::OwnerResponse;
-use cosmwasm_std::entry_point;
-use cosmwasm_std::Addr;
+use cosmwasm_std::{entry_point};
+use cosmwasm_std::{Addr, Coin};
 use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 
 use crate::error::ContractError;
 use crate::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{State, STATE};
+use crate::state::{STATE, State, Bidder, Bid};
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -15,9 +15,22 @@ pub fn instantiate(
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    let state = State {
-        count: msg.count,
+
+    let sender = Addr::unchecked("sender");
+    let funds: Bid = Bid {
+        amount: Coin::new(0, "atom"),
+    };
+
+    let state = &State {
+    
+
+       highest_bid: Bidder { 
+            sender: sender,
+            total_bid: funds,
+            transfer_addr: Some(sender), 
+    },
         owner: info.sender.clone(),
+        closed_bidding: false,
     };
 
     STATE.save(deps.storage, &state)?;
@@ -25,7 +38,7 @@ pub fn instantiate(
     Ok(Response::new()
         .add_attribute("method", "instantiate")
         .add_attribute("owner", info.sender)
-        .add_attribute("count", msg.count.to_string()))
+    )
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -109,103 +122,103 @@ fn query_owner(deps: Deps) -> StdResult<OwnerResponse> {
     Ok(OwnerResponse { owner: state.owner })
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+//     use cosmwasm_std::{coins, from_binary};
 
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies();
+//     #[test]
+//     fn proper_initialization() {
+//         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
+//         let msg = InstantiateMsg { count: 17 };
+//         let info = mock_info("creator", &coins(1000, "earth"));
 
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
+//         // we can just call .unwrap() to assert this was a success
+//         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         assert_eq!(0, res.messages.len());
 
-        // it worked, let's query the count state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
+//         // it worked, let's query the count state
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: CountResponse = from_binary(&res).unwrap();
+//         assert_eq!(17, value.count);
 
-        // it worked, let's query the owner state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
-        let value: OwnerResponse = from_binary(&res).unwrap();
-        assert_eq!("creator", value.owner);
-    }
+//         // it worked, let's query the owner state
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
+//         let value: OwnerResponse = from_binary(&res).unwrap();
+//         assert_eq!("creator", value.owner);
+//     }
 
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies();
+//     #[test]
+//     fn increment() {
+//         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         let msg = InstantiateMsg { count: 17 };
+//         let info = mock_info("creator", &coins(1000, "earth"));
+//         let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // anyone can increment counter
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         // anyone can increment counter
+//         let info = mock_info("anyone", &coins(2, "token"));
+//         let msg = ExecuteMsg::Increment {};
+//         let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
+//         // should increase counter by 1
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: CountResponse = from_binary(&res).unwrap();
+//         assert_eq!(18, value.count);
+//     }
 
-    #[test]
-    fn change_owner() {
-        let mut deps = mock_dependencies();
+//     #[test]
+//     fn change_owner() {
+//         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 12 };
-        let info = mock_info("creator", &coins(1000, "earth"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         let msg = InstantiateMsg { count: 12 };
+//         let info = mock_info("creator", &coins(1000, "earth"));
+//         let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // owner should be creator
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
-        let value: OwnerResponse = from_binary(&res).unwrap();
-        assert_eq!("creator", value.owner);
+//         // owner should be creator
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
+//         let value: OwnerResponse = from_binary(&res).unwrap();
+//         assert_eq!("creator", value.owner);
 
-        // contract creator can transfer contract to new owner
-        let info = mock_info("creator", &coins(2, "token"));
-        let address = Addr::unchecked("anyone");
-        let msg = ExecuteMsg::NewOwner { new_owner: address };
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         // contract creator can transfer contract to new owner
+//         let info = mock_info("creator", &coins(2, "token"));
+//         let address = Addr::unchecked("anyone");
+//         let msg = ExecuteMsg::NewOwner { new_owner: address };
+//         let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // owner should be anyone
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
-        let value: OwnerResponse = from_binary(&res).unwrap();
-        assert_eq!("anyone", value.owner)
-    }
+//         // owner should be anyone
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
+//         let value: OwnerResponse = from_binary(&res).unwrap();
+//         assert_eq!("anyone", value.owner)
+//     }
 
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies();
+//     #[test]
+//     fn reset() {
+//         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+//         let msg = InstantiateMsg { count: 17 };
+//         let info = mock_info("creator", &coins(1000, "earth"));
+//         let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // unauthorized attempt to reset the counter by anyone
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
+//         // unauthorized attempt to reset the counter by anyone
+//         let unauth_info = mock_info("anyone", &coins(2, "token"));
+//         let msg = ExecuteMsg::Reset { count: 5 };
+//         let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
+//         match res {
+//             Err(ContractError::Unauthorized {}) => {}
+//             _ => panic!("Must return unauthorized error"),
+//         }
 
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+//         // only the original creator can reset the counter
+//         let auth_info = mock_info("creator", &coins(2, "token"));
+//         let msg = ExecuteMsg::Reset { count: 5 };
+//         let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
 
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
-    }
-}
+//         // should now be 5
+//         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: CountResponse = from_binary(&res).unwrap();
+//         assert_eq!(5, value.count);
+//     }
+// }
--- src/msg.rs
+++ src/msg.rs
@@ -2,36 +2,40 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_schema::QueryResponses;
 use cosmwasm_std::Addr;
 
+use crate::state::{Bidder};
+
 #[cw_serde]
 pub struct InstantiateMsg {
-    pub count: i32,
+    pub higest_bid: Bidder,
 }
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    Increment {},
-    NewOwner { new_owner: Addr },
-    Reset { count: i32 },
+    Bid {},
+    AddTranserAddr {},
+    Retract { new_owner: Addr },
+    CloseBidding { count: i32 },
+
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
-    #[returns(CountResponse)]
-    GetCount {},
-    #[returns(OwnerResponse)]
-    GetOwner {},
+    #[returns(HighestBidResponse)]
+    GetHighestBid {},
+    #[returns(HighestBidderResponse)]
+    GetHighestBidder {},
 }
 
 // We define a custom struct for each query response
 #[cw_serde]
-pub struct CountResponse {
+pub struct HighestBidResponse {
     pub count: i32,
 }
 
 // We define a custom struct for each query response
 #[cw_serde]
-pub struct OwnerResponse {
+pub struct HighestBidderResponse {
     pub owner: Addr,
 }
